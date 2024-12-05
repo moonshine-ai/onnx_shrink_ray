9d6f07a5c77f7724f07c09431f7d7bc4e6c6e0df
Pete Warden <pete@usefulsensors.com>
2024-12-04 16:19:09 -0800

    Initial implementation of weight quantization

---
+++ onnx_weight_quantization.py
@@ -0,0 +1,122 @@
+import onnx_graphsurgeon as gs
+import numpy as np
+import onnx
+
+# Don't quantify constants smaller than this.
+DEFAULT_MIN_ELEMENTS = 16 * 1024
+
+def quantize_tensor(name, value_tensor, original_output_tensor_name, graph):
+    # print(f"Quantizing {name} with {elements} elements.")
+    float_values = value_tensor.values
+    min_val = np.min(float_values)
+    max_val = np.max(float_values)
+    range_val = max_val - min_val
+    inverse_range = 1.0 / range_val
+    zero_point = round(-min_val * inverse_range * 255.0) - 128
+    # y = (x - zero_point) * scale
+    # print(f"Min: {min_val}, Max: {max_val}, Range: {range_val}, Inverse Range: {inverse_range}, Zero Point: {zero_point}")
+    quantized_values = np.round(float_values * inverse_range * 255.0) + zero_point
+    # print(f"Quantized values: {quantized_values}")
+    quantized_values = np.clip(quantized_values, -128, 127).astype(np.int8)
+
+    quantized_tensor = gs.Constant(
+        name=f"{name}_quantized", 
+        values=quantized_values)
+    
+    zero_point_tensor = gs.Constant(
+        name=f"{name}_zero_point", 
+        values=np.array([zero_point], dtype=np.int8))
+
+    scale_value = range_val / 255.0        
+    scale_tensor = gs.Constant(
+        name=f"{name}_scale",
+        values=np.array([scale_value], dtype=np.float32))
+    
+    dequantized_tensor_name = f"{name}_dequantized_tensor"
+    dequantized_tensor = gs.Variable(
+        name=dequantized_tensor_name, 
+        dtype=np.float32,
+        shape=value_tensor.shape)
+
+    dequantized_node = gs.Node(
+        op="DequantizeLinear", 
+        name=f"{name}_dequantized_node", 
+        inputs=[quantized_tensor, scale_tensor, zero_point_tensor],
+        outputs=[dequantized_tensor])
+
+    for node in graph.nodes:
+        for i, tensor in enumerate(node.inputs):
+            if tensor.name == original_output_tensor_name:
+                node.inputs[i] = dequantized_tensor
+
+    for i, tensor in enumerate(graph.outputs):
+        if tensor.name == original_output_tensor_name:
+            graph.outputs[i] = dequantized_tensor
+
+    # Add the quantized tensor to the graph.
+    graph.nodes.append(dequantized_node)
+
+
+def quantize_weights(model, min_elements=DEFAULT_MIN_ELEMENTS):
+    graph = gs.import_onnx(model)
+
+    original_graph = graph.copy()
+
+    # Quantize the weights
+    for node in original_graph.nodes:
+        if node.op != "Constant":
+            # print(f"Node {node.name} is not a Constant node. Skipping quantization.")
+            continue
+        name = node.name
+        value_tensor = node.attrs["value"]
+        original_output_tensor_name = node.outputs[0].name
+        elements = np.prod(value_tensor.shape)
+        if elements < min_elements:
+            # print(f"Not quantizing {name} with {elements} elements.")
+            continue
+        quantize_tensor(name, value_tensor, original_output_tensor_name, graph)
+
+    for name, value_tensor in original_graph.tensors().items():
+        if value_tensor.__class__ != gs.Constant:
+            continue
+        original_output_tensor_name = name
+        elements = np.prod(value_tensor.shape)
+        if elements < min_elements:
+            # print(f"Not quantizing {name} with {elements} elements.")
+            continue
+        quantize_tensor(name, value_tensor, original_output_tensor_name, graph)
+    
+    graph.cleanup(remove_unused_graph_inputs=True).toposort()
+
+    no_shape_model = gs.export_onnx(graph)
+    new_model = onnx.shape_inference.infer_shapes(no_shape_model)
+
+    onnx.checker.check_model(new_model)
+    
+    return new_model
+
+
+if __name__ == "__main__":
+    import glob
+    import os
+    import sys
+
+    if len(sys.argv) < 2:
+        input_globs = ["*.onnx"]
+    else:
+        input_globs = sys.argv[1:]
+
+    for input_glob in input_globs:
+        input_filenames = list(glob.glob(input_glob))
+        if len(input_filenames) == 0:
+            print(f"No files found matching '{input_glob}'.")
+            sys.exit(1)
+
+        for input_filename in input_filenames:
+            if input_filename.endswith("_quantized_weights.onnx"):
+                print(f"Skipping {input_filename} as it is already quantized.")
+                continue
+            original_model = onnx.load(input_filename)
+            new_model = quantize_weights(original_model)
+            output_filename = os.path.splitext(input_filename)[0] + "_quantized_weights.onnx"
+            onnx.save(new_model, output_filename)
---
+++ onnx_weight_quantization_test.py
@@ -0,0 +1,171 @@
+import onnx
+from onnx import helper as h, TensorProto as tp
+import onnxruntime as ort
+import numpy as np
+
+from onnx_weight_quantization import quantize_weights
+
+def check_quantization(float_model):
+    onnx.checker.check_model(float_model)
+
+    quantized_model = quantize_weights(float_model, min_elements=2)
+
+    float_session = ort.InferenceSession(float_model.SerializeToString())
+    actual_float_output = np.array(
+        float_session.run(None, {})[0], 
+        dtype=np.float32)
+
+    quantized_session = ort.InferenceSession(quantized_model.SerializeToString())
+    actual_quantized_output = np.array(
+        quantized_session.run(None, {})[0], 
+        dtype=np.float32)
+
+    output_min = np.min(actual_float_output)
+    output_max = np.max(actual_float_output)
+    output_range = output_max - output_min
+    output_bin_size = output_range / 255.0
+    
+    # print(f"actual_float_output: {actual_float_output}")
+    # print(f"actual_quantized_output: {actual_quantized_output}")
+    
+    output_diff = np.abs(actual_float_output - actual_quantized_output)
+    max_diff = np.max(output_diff)
+    if max_diff > output_bin_size:
+        raise Exception(f"Max difference {max_diff} is greater than output bin size {output_bin_size}.")
+
+def test_single_constant():
+    weights_shape = (1, 1, 2, 2)
+    weights_values = np.array([[[[0.0, 2.5], [5.0, 10.0]]]], dtype=np.float32)
+
+    weights_tensor = h.make_tensor(name="weights_tensor", data_type=tp.FLOAT,
+        dims=weights_shape,
+        vals=weights_values)
+
+    weights_node = h.make_node("Constant", inputs=[], outputs=["weights_output"], name="weights_node",
+        value=weights_tensor)
+
+    float_graph = h.make_graph([weights_node], "test_graph",
+        [],
+        [h.make_tensor_value_info("weights_output", tp.FLOAT, weights_shape)])
+
+    float_model = h.make_model(float_graph, producer_name="quantization_test")
+
+    check_quantization(float_model)
+
+def test_identity():
+    weights_shape = (1, 1, 2, 2)
+    weights_values = np.array([[[[0.0, 2.5], [5.0, 10.0]]]], dtype=np.float32)
+
+    weights_tensor = h.make_tensor(name="weights_tensor", data_type=tp.FLOAT,
+        dims=weights_shape,
+        vals=weights_values)
+
+    weights_node = h.make_node("Constant", inputs=[], outputs=["weights_output"], name="weights_node",
+        value=weights_tensor)
+
+    identity_node = h.make_node("Identity", inputs=["weights_output"], outputs=["identity_output"], name="identity_node")
+
+    float_graph = h.make_graph([weights_node, identity_node], "test_graph",
+        [],
+        [h.make_tensor_value_info("identity_output", tp.FLOAT, weights_shape)])
+
+    float_model = h.make_model(float_graph, producer_name="quantization_test")
+
+    check_quantization(float_model)
+
+def test_mul():
+    weights_shape = (1, 1, 2, 2)
+    weights_values = np.array([[[[0.0, 2.5], [5.0, 10.0]]]], dtype=np.float32)
+    weights_tensor = h.make_tensor(name="weights_tensor", data_type=tp.FLOAT,
+        dims=weights_shape,
+        vals=weights_values)
+    weights_node = h.make_node("Constant", inputs=[], outputs=["weights_output"], name="weights_node",
+        value=weights_tensor)
+
+    two_shape = (1, )
+    two_values = np.array([2.0], dtype=np.float32)
+    two_tensor = h.make_tensor(name="two_tensor", data_type=tp.FLOAT,
+        dims=two_shape,
+        vals=two_values)
+    two_node = h.make_node("Constant", inputs=[], outputs=["two_output"], name="two_node",
+        value=two_tensor)
+
+    mul_node = h.make_node("Mul", inputs=["weights_output", "two_output"], outputs=["mul_output"], name="mul_node")
+
+    float_graph = h.make_graph([weights_node, two_node, mul_node], "test_graph",
+        [],
+        [h.make_tensor_value_info("mul_output", tp.FLOAT, weights_shape)])
+
+    float_model = h.make_model(float_graph, producer_name="quantization_test")
+
+    check_quantization(float_model)
+
+def test_large_constant():
+    weights_width = 256
+    weights_height = 256
+    weights_shape = (1, 1, weights_height, weights_width)
+    rng = np.random.default_rng(7528840384)
+    weights_values = rng.random((weights_shape)).astype(np.float32)
+
+    weights_tensor = h.make_tensor(name="weights_tensor", data_type=tp.FLOAT,
+        dims=weights_shape,
+        vals=weights_values)
+
+    weights_node = h.make_node("Constant", inputs=[], outputs=["weights_output"], name="weights_node",
+        value=weights_tensor)
+
+    float_graph = h.make_graph([weights_node], "test_graph",
+        [],
+        [h.make_tensor_value_info("weights_output", tp.FLOAT, weights_shape)])
+
+    float_model = h.make_model(float_graph, producer_name="quantization_test")
+
+    check_quantization(float_model)
+
+def test_signed_constant():
+    weights_shape = (1, 1, 2, 2)
+    weights_values = np.array([[[[-5.0, -2.5], [0.0, 5.0]]]], dtype=np.float32)
+
+    weights_tensor = h.make_tensor(name="weights_tensor", data_type=tp.FLOAT,
+        dims=weights_shape,
+        vals=weights_values)
+
+    weights_node = h.make_node("Constant", inputs=[], outputs=["weights_output"], name="weights_node",
+        value=weights_tensor)
+
+    float_graph = h.make_graph([weights_node], "test_graph",
+        [],
+        [h.make_tensor_value_info("weights_output", tp.FLOAT, weights_shape)])
+
+    float_model = h.make_model(float_graph, producer_name="quantization_test")
+
+    check_quantization(float_model)
+
+
+def test_unbalanced_constant():
+    weights_shape = (1, 1, 2, 2)
+    weights_values = np.array([[[[-2.0, 0.5], [3.0, 8.0]]]], dtype=np.float32)
+
+    weights_tensor = h.make_tensor(name="weights_tensor", data_type=tp.FLOAT,
+        dims=weights_shape,
+        vals=weights_values)
+
+    weights_node = h.make_node("Constant", inputs=[], outputs=["weights_output"], name="weights_node",
+        value=weights_tensor)
+
+    float_graph = h.make_graph([weights_node], "test_graph",
+        [],
+        [h.make_tensor_value_info("weights_output", tp.FLOAT, weights_shape)])
+
+    float_model = h.make_model(float_graph, producer_name="quantization_test")
+
+    check_quantization(float_model)
+
+if __name__ == "__main__":
+    test_single_constant()
+    test_identity()
+    test_mul()
+    test_large_constant()
+    test_signed_constant()
+    test_unbalanced_constant()
+    print("All tests passed.")
