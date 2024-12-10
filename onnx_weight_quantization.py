87511569ba7ab5b8503779dc4e5e6396b44745bb
Pete Warden <pete@usefulsensors.com>
2024-12-10 10:47:04 -0800

    Implemented flexible float quantization and better arg parsing

--- onnx_weight_quantization.py
+++ onnx_weight_quantization.py
@@ -62,13 +62,12 @@ def float_quantize_node(name, value_tensor, original_output_tensor_name, graph, 
     max_val = np.max(float_values)
     range_val = max_val - min_val
     inverse_range = 1.0 / range_val
-    zero_point = round(-min_val * inverse_range * 255.0) - 128
-    scale_value = range_val / 255.0        
-    # y = (x - zero_point) * scale
-    # print(f"Min: {min_val}, Max: {max_val}, Range: {range_val}, Inverse Range: {inverse_range}, Zero Point: {zero_point}")
-    quantized_values = np.round(float_values * inverse_range * 255.0) + zero_point
+    half_levels = (levels / 2)
+    zero_point = round(-min_val * inverse_range * (levels - 1)) - half_levels
+    scale_value = range_val / (levels - 1)
+    quantized_values = np.round(float_values * inverse_range * (levels - 1)) + zero_point
     # print(f"Quantized values: {quantized_values}")
-    quantized_values = np.clip(quantized_values, -128, 127).astype(np.int8)
+    quantized_values = np.clip(quantized_values, -half_levels, (half_levels - 1))
     dequantized_values = ((quantized_values.astype(np.int32) - zero_point) * scale_value).astype(np.float32)
     # print(f"Dequantized values: {dequantized_values}")
 
@@ -85,7 +84,7 @@ def float_quantize_node(name, value_tensor, original_output_tensor_name, graph, 
         if tensor.name == original_output_tensor_name:
             graph.outputs[i] = dequantized_tensor
 
-def quantize_weights(model, min_elements=DEFAULT_MIN_ELEMENTS, float_quantization=False):
+def quantize_weights(model, min_elements=DEFAULT_MIN_ELEMENTS, float_quantization=False, float_levels=256):
     graph = gs.import_onnx(model)
 
     original_graph = graph.copy()
@@ -103,7 +102,7 @@ def quantize_weights(model, min_elements=DEFAULT_MIN_ELEMENTS, float_quantizatio
             # print(f"Not quantizing {name} with {elements} elements.")
             continue
         if float_quantization:
-            float_quantize_node(name, value_tensor, original_output_tensor_name, graph)
+            float_quantize_node(name, value_tensor, original_output_tensor_name, graph, levels=float_levels)
         else:
             quantize_tensor(name, value_tensor, original_output_tensor_name, graph)
 
@@ -116,7 +115,7 @@ def quantize_weights(model, min_elements=DEFAULT_MIN_ELEMENTS, float_quantizatio
             # print(f"Not quantizing {name} with {elements} elements.")
             continue
         if float_quantization:           
-            float_quantize_node(name, value_tensor, original_output_tensor_name, graph)
+            float_quantize_node(name, value_tensor, original_output_tensor_name, graph, levels=float_levels)
         else:
             quantize_tensor(name, value_tensor, original_output_tensor_name, graph)
     
@@ -131,47 +130,74 @@ def quantize_weights(model, min_elements=DEFAULT_MIN_ELEMENTS, float_quantizatio
 
 
 if __name__ == "__main__":
+    import argparse
     import glob
     import os
     import sys
 
+    parser = argparse.ArgumentParser(
+        prog=sys.argv[0],
+        description="Quantization utility for ONNX models",
+    )
+    parser.add_argument(
+        "--method", "-m",
+        help="How to quantize the models",
+        default="integer_weights",
+        choices=["integer_weights", "float_weights", "integer_activations"],
+    )
+    parser.add_argument(
+        "--float_levels", "-l",
+        help="Number of levels to use for float quantization.",
+        default=256,
+        type=int,
+    )
+    parser.add_argument(
+        "--output_dir", "-o",
+        help="Folder to write the quantized models to. If not specified, uses the same folder as the input models.",
+        default=None,
+    )
+    parser.add_argument(
+        "--output_suffix", "-s",
+        help="Suffix to add to the output model filenames.",
+        default="quantized_weights.onnx",
+    )
+    parser.add_argument("globs", nargs="*")
+    args = parser.parse_args()
+    if len(args.globs) == 0:
+        args.globs = ["*.onnx"]
 
-    if len(sys.argv) < 2:
-        input_globs = ["*.onnx"]
-    else:
-        all_args = sys.argv[1:]
-        input_globs = []
-        method = "integer_weights"
-        for arg in all_args:
-            if arg.startswith("-"):
-                if arg == "-f" or arg == "--float":
-                    method = "float_weights"
-                elif arg == "-a" or arg == "--activations":
-                    method = "integer_activations"
-                else:
-                    print(f"Unknown option: {arg}")
-                    sys.exit(1)
-            else:
-                input_globs.append(arg)
+    if args.output_dir is not None and not os.path.isdir(args.output_dir):
+        os.makedirs(args.output_dir)
 
-    for input_glob in input_globs:
+    for input_glob in args.globs:
+        if os.path.isdir(input_glob):
+            input_glob = os.path.join(input_glob, "*.onnx")
         input_filenames = list(glob.glob(input_glob))
         if len(input_filenames) == 0:
             print(f"No files found matching '{input_glob}'.")
             sys.exit(1)
 
         for input_filename in input_filenames:
-            if input_filename.endswith("_quantized_weights.onnx"):
-                print(f"Skipping {input_filename} as it is already quantized.")
+            if args.output_suffix != ".onnx" and input_filename.endswith(args.output_suffix):
+                print(f"Skipping '{input_filename}' as it is already quantized.")
                 continue
-            output_filename = os.path.splitext(input_filename)[0] + "_quantized_weights.onnx"
-            if method == "float_weights" or method == "integer_weights":
+            input_base = os.path.basename(input_filename)
+            input_dir = os.path.dirname(input_filename)
+            output_base = os.path.splitext(input_base)[0] + args.output_suffix
+            if args.output_dir is None:
+                output_filename = os.path.join(input_dir, output_base)
+            else:
+                output_filename = os.path.join(args.output_dir, output_base)
+            if output_filename == input_filename:
+                print(f"Skipping '{input_filename}' as the output filename is the same and it would be overwritten.")
+                continue
+            if args.method == "float_weights" or args.method == "integer_weights":
                 original_model = onnx.load(input_filename)
-                float_quantization = (method == "float_weights")
-                new_model = quantize_weights(original_model, float_quantization=float_quantization)
+                float_quantization = (args.method == "float_weights")
+                new_model = quantize_weights(original_model, float_quantization=float_quantization, float_levels=args.float_levels)
                 onnx.save(new_model, output_filename)
-            elif method == "integer_activations":
+            elif args.method == "integer_activations":
                 quantize_dynamic(input_filename, output_filename, weight_type=QuantType.QUInt8)
             else:
-                print(f"Unknown quantization method: {method}")
+                print(f"Unknown quantization method: {args.method}")
                 sys.exit(1)
